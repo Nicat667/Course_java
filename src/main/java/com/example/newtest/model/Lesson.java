25cb0d0dcb24a487f5b6081558bacc583f04a6fc
Nicat667 <nijatvo@code.edu.az>
2025-10-28 17:22:37 +0400

    course_app project created

---
+++ src/main/java/com/example/newtest/model/Lesson.java
@@ -0,0 +1,23 @@
+package com.example.newtest.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+}
