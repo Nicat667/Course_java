a7a482bae0889174d25407206de1119795da96e8
Nicat667 <nijatvo@code.edu.az>
2025-11-02 02:01:51 +0400

    Methods od student service are created

--- src/main/java/com/example/newtest/repository/LessonRepository.java
+++ src/main/java/com/example/newtest/repository/LessonRepository.java
@@ -3,5 +3,7 @@ package com.example.newtest.repository;
 import com.example.newtest.model.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LessonRepository extends JpaRepository<Lesson, Integer> {
 }
