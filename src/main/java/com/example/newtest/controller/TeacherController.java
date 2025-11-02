81a21c3dd09aacbc938fd3cd0104025081522a38
Nicat667 <nijatvo@code.edu.az>
2025-11-02 17:26:51 +0400

    methods of teacher controller are created

---
+++ src/main/java/com/example/newtest/controller/TeacherController.java
@@ -0,0 +1,62 @@
+package com.example.newtest.controller;
+
+import com.example.newtest.model.Lesson;
+import com.example.newtest.model.Teacher;
+import com.example.newtest.repository.TeacherRepository;
+import com.example.newtest.service.TeacherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/teachers")
+public class TeacherController {
+
+    private final TeacherService teacherService;
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Teacher>> getAllTeachers() {
+        return ResponseEntity.ok(teacherService.GetAllTeachers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Teacher> getTeacherById(@PathVariable int id) {
+        return ResponseEntity.ok(teacherService.GetTeacherById(id));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Teacher> addTeacher(@RequestBody Teacher teacher) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(teacherService.CreateNewTeacher(teacher));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteTeacher(@PathVariable int id) {
+        teacherService.DeleteTeacherById(id);
+        return ResponseEntity.ok("Teacher is deleted");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Teacher> updateTeacher(@RequestBody Teacher teacher, @PathVariable int id) {
+        return ResponseEntity.ok(teacherService.UpdateTeacherById(id, teacher));
+    }
+
+    @GetMapping("/getLessons/{id}")
+    public ResponseEntity<Set<Lesson>> getLessonsById(@PathVariable int id) {
+        return ResponseEntity.ok(teacherService.GetLessonsByTeacherId(id));
+    }
+
+    @PutMapping("/addLesson/{teacherId}/{lessonId}")
+    public ResponseEntity<Teacher> addLesson(@PathVariable int lessonId, @PathVariable int teacherId) {
+        return ResponseEntity.ok(teacherService.AddLessonToTeacher(teacherId, lessonId));
+    }
+
+    @PutMapping("/removeLesson/{teacherId}/{lessonId}")
+    public ResponseEntity<Teacher> removeLesson(@PathVariable int lessonId, @PathVariable int teacherId) {
+        return ResponseEntity.ok(teacherService.DeleteLessonFromTeacher(teacherId, lessonId));
+    }
+}
