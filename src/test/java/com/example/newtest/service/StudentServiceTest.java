2d9231c168210cc06bcf8dfeb3f08848c890caf7
Nicat667 <nijatvo@code.edu.az>
2025-11-12 02:42:49 +0400

    student service tests are added

---
+++ src/test/java/com/example/newtest/service/StudentServiceTest.java
@@ -0,0 +1,68 @@
+package com.example.newtest.service;
+
+import com.example.newtest.model.Lesson;
+import com.example.newtest.model.Student;
+import com.example.newtest.repository.StudentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+public class StudentServiceTest {
+
+    @Mock
+    private StudentRepository studentRepository;
+
+    @InjectMocks
+    private StudentService studentService;
+
+    private Student student;
+
+    @BeforeEach
+    public void setUp()
+    {
+        MockitoAnnotations.openMocks(this);
+        student = new Student(1, "Alice", "alice@mail.com", new HashSet<>());
+    }
+
+    @Test
+    void getStudent_ReturnsStudent() {
+        when(studentRepository.findById(1)).thenReturn(Optional.of(student));
+        Student result = studentService.getStudent(1);
+        assertEquals("Alice", result.getName());
+    }
+
+    @Test
+    void deleteStudent_RemovesStudent() {
+        Lesson lesson = new Lesson();
+        lesson.setStudents(new HashSet<>());
+        Set<Lesson> lessons = new HashSet<>();
+        lessons.add(lesson);
+        student.setLessons(lessons);
+
+        when(studentRepository.findById(1)).thenReturn(Optional.of(student));
+
+        studentService.deleteStudent(1);
+
+        verify(studentRepository).deleteById(1);
+    }
+
+//    @Test
+//    void getAllLessonsOfStudent_ReturnsLessons() {
+//        Lesson lesson = new Lesson();
+//        student.getLessons().add(lesson);
+//        when(studentRepository.findById(1)).thenReturn(Optional.of(student));
+//        Set<Lesson> lessons = studentService.getAllLessonsOfStudent(1);
+//        assertTrue(lessons.contains(lesson));
+//    }
+}
